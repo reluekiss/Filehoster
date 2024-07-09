74fe5a187d24df1af7e9aa5f152d8993a89fc076
relue <natalierogers048@gmail.com>
2024-07-09 14:07:21 -0400

    changed cache obsoletion from last accessed to least frequently accessed

--- src/cache.c
+++ src/cache.c
@@ -1,8 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <openssl/evp.h>
 #include "hashtable.h"
 #include "cache.h"
+
+// MD5 hashing by devtty1er https://stackoverflow.com/questions/7627723/how-to-create-a-md5-hash-of-a-string-in-c
+void bytes2md5(const char *data, int len, char *md5buf) {
+  // Based on https://www.openssl.org/docs/manmaster/man3/EVP_DigestUpdate.html
+  EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
+  const EVP_MD *md = EVP_md5();
+  unsigned char md_value[EVP_MAX_MD_SIZE];
+  unsigned int md_len, i;
+  EVP_DigestInit_ex(mdctx, md, NULL);
+  EVP_DigestUpdate(mdctx, data, len);
+  EVP_DigestFinal_ex(mdctx, md_value, &md_len);
+  EVP_MD_CTX_free(mdctx);
+  for (i = 0; i < md_len; i++) {
+    snprintf(&(md5buf[i * 2]), 16 * 2, "%02x", md_value[i]);
+  }
+}
+
+// Find ce with lowest hit count
+struct cache_entry* lowest_hit_entry(struct cache *cache) {
+    int size = cache->cur_size;
+    struct cache_entry *slow_ptr = cache->head, *fast_ptr = cache->head;
+    int lowest = INT_MAX;
+
+    while (fast_ptr != NULL && fast_ptr->next != NULL) {
+        fast_ptr = fast_ptr->next->next;
+        slow_ptr = slow_ptr->next;
+
+        if (fast_ptr == NULL || fast_ptr->next == NULL) {
+            break;
+        }
+
+        if (slow_ptr->hits < lowest) {
+            lowest = slow_ptr->hits;
+        } else if (slow_ptr->hits == lowest && slow_ptr->hits > 0) { 
+            lowest = slow_ptr->hits;
+        }
+    }
+    return slow_ptr;
+}
+
+// Reset hits in cache
+void reset_hit_counts(struct cache *cache) {
+    struct cache_entry *current = cache->head;
+    while (current != NULL) {
+        current->hits = 0; // Reset hits to 0
+        current = current->next;
+    }
+}
 
 /**
  * Allocate a cache entry
@@ -45,8 +94,9 @@ void dllist_insert_head(struct cache *cache, struct cache_entry *ce) {
 
 /**
  * Move a cache entry to the head of the list
+ * NOTE: Obsolete by using hit frequency with floyd cycle finding algorithm
  */
-void dllist_move_to_head(struct cache *cache, struct cache_entry *ce) {
+/* void dllist_move_to_head(struct cache *cache, struct cache_entry *ce) {
     if (ce != cache->head) {
         if (ce == cache->tail) {
             // We're the tail
@@ -64,23 +114,19 @@ void dllist_move_to_head(struct cache *cache, struct cache_entry *ce) {
         ce->prev = NULL;
         cache->head = ce;
     }
-}
-
+} */
 
 /**
- * Removes the tail from the list and returns it
+ * Removes an from the list and returns it
  *
- * NOTE: does not deallocate the tail
  */
-struct cache_entry *dllist_remove_tail(struct cache *cache) {
-    struct cache_entry *oldtail = cache->tail;
-
-    cache->tail = oldtail->prev;
-    cache->tail->next = NULL;
+void dllist_remove_entry(struct cache *cache, struct cache_entry *ce) {
+    ce->next->prev = ce->prev;
+    ce->prev->next = ce->next;
 
     cache->cur_size--;
 
-    return oldtail;
+    free(ce);
 }
 
 /**
@@ -127,8 +173,7 @@ void cache_put(struct cache *cache, char *path, char *content_type, void *conten
     struct cache_entry *ce = alloc_entry(path, content_type, content, content_length);
     
     if(cache->cur_size >= cache->max_size)
-        free(dllist_remove_tail(cache));
-    
+        dllist_remove_entry(cache, lowest_hit_entry(cache));
     dllist_insert_head(cache, ce);
     hashtable_put(cache->index, path, ce);
     
@@ -140,7 +185,7 @@ void cache_put(struct cache *cache, char *path, char *content_type, void *conten
  */
 struct cache_entry *cache_get(struct cache *cache, char *path) {
     struct cache_entry *ce = hashtable_get(cache->index, path);
-    dllist_move_to_head(cache, ce);
+    //dllist_move_to_head(cache, ce);
     ce->hits++;
     return ce;
 }
